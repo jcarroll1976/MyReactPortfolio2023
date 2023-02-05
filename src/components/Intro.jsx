74ac278ec571efd6dbd03aa15bd101f6db7cc571
Joshua Carroll <carrolljosh1976@gmail.com>
2023-02-05 10:57:44 -0500

    Added components and css

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import "./intro.scss";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 function Intro() {
+
+  /*const [text,count] =useTypewriter({
+    words: [
+        "Full Stack JavaScript Developer",
+        "Guy-who-loves-Coffee.tsx",
+        "<ButLovesToCodeMore/>",
+    ],
+    loop:true,
+    delaySpeed:2000,
+});*/
+
   return (
     <div className="intro" id="intro">
-        
+        <div className="left">
+          <div className="imgContainer">
+            <img src="assets/Profilepic.jpg" alt="" />
+          </div>
+        </div>
+        <div className="right">
+          <div className="wrapper">
+            <h2>Hi there, I'm</h2>
+            <h1>Joshua Carroll</h1>
+            <h3>Full Stack JavaScript Developer</h3>
+          </div>
+          <a href="#portfolio">
+            <KeyboardArrowDownIcon className="introImg" />
+          </a>
+        </div>
     </div>
   )
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,61 @@
 import React from 'react';
+import { useState } from 'react';
 import "./portfolio.scss";
+import PortfolioList from './PortfolioList';
 
 function Portfolio() {
+
+  const [selected,setSelected] = useState("featured");
+
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    }
+  ]
+
   return (
     <div className="portfolio" id="portfolio">
-        
+        <h1>Portfolio</h1>
+        <ul>
+          {list.map((item) => (
+              <PortfolioList title = {item.title} active = {selected === item.id} setSelected = {setSelected} id = {item.id} />
+          ))}
+        </ul>
+        <div className="container">
+          <div className="item">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt="" />
+            <h3>Banking App</h3>
+          </div>
+        </div>
     </div>
   )
 }
