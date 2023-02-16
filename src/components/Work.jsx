3b27a2b4af5c0ec4c7113973d7b61e81e40ebf1f
Joshua Carroll <carrolljosh1976@gmail.com>
2023-02-15 22:37:24 -0500

    Added image for imgContainer

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -20,7 +20,12 @@ function Work() {
                 <span>Projects</span>
               </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+            <img
+                  src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+                  alt=""
+                />
+            </div>
           </div>
         </div>
       </div>
