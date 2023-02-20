c83c01f05185c4338aa7fc5353b6a477904e62e2
Joshua Carroll <carrolljosh1976@gmail.com>
2023-02-19 21:48:18 -0500

    Added to Testimoinals component

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,7 +4,24 @@ import "./testimonials.scss"
 function Testimonials() {
   return (
     <div className="testimonials" id="testimonials">
-        
+        <h1>Testimonials</h1>
+        <div className="container">
+          <div className="card">
+            <div className="top">
+              <img src="assets/right-arrow.png" alt="" />
+              <img src="https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
+              <img src="assets/youtube.png" />
+            </div>
+            <div className="center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+              ullam nemo, in aspernatur aut fuga.
+            </div>
+            <div className="bottom">
+              <h3>Alex</h3>
+              <h4>CEO of LAMA</h4>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
