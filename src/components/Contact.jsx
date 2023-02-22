cb791899b183cfa6006094ef7a5043e5fd7dee83
Joshua Carroll <carrolljosh1976@gmail.com>
2023-02-22 12:07:49 -0500

    Added content and styling to Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 import "./contact.scss";
 
 function Contact() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   return (
     <div className="contact" id="contact">
-        
+        <div className="left">
+          <img src="assets/shake.svg" alt="" />
+        </div>
+        <div className="right">
+          <h2>Contact.</h2>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Email"/>
+            <textarea placeholder="Message"/>
+            <button type="submit">Send</button>
+          </form>
+        </div>
     </div>
   )
 }
